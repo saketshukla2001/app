55691e84413dd4d8efafc052560da47ffc8c74a6
saketshukla2001 <saketshukla078@gmail.com>
2025-01-13 18:13:37 +0530

    setuped

--- 6amMart_React/public/firebase-messaging-sw.js
+++ 6amMart_React/public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts(
 );
 // // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyBqERf339BL4OIXfx4NXvY_-ZjZsH7N2OY",
+  authDomain: "nukkad-deb3b.firebaseapp.com",
+  projectId: "nukkad-deb3b",
+  storageBucket: "nukkad-deb3b.firebasestorage.app",
+  messagingSenderId: "158653924415",
+  appId: "1:158653924415:web:7c4f0a10f58cb33397248a",
+  measurementId: "G-XH0RHTSXZW",
 };
 
 firebase?.initializeApp(firebaseConfig);
--- 6amMart_React/src/firebase.js
+++ 6amMart_React/src/firebase.js
@@ -7,13 +7,13 @@ import {
 } from "firebase/messaging";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyBqERf339BL4OIXfx4NXvY_-ZjZsH7N2OY",
+  authDomain: "nukkad-deb3b.firebaseapp.com",
+  projectId: "nukkad-deb3b",
+  storageBucket: "nukkad-deb3b.firebasestorage.app",
+  messagingSenderId: "158653924415",
+  appId: "1:158653924415:web:7c4f0a10f58cb33397248a",
+  measurementId: "G-XH0RHTSXZW",
 };
 const firebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
@@ -32,7 +32,7 @@ const messaging = (async () => {
 
 export const fetchToken = async (setTokenFound, setFcmToken) => {
   return getToken(await messaging, {
-    vapidKey: "",
+    vapidKey: "BIyuB12perX-qVRU3AYj3OPOBoihnSRpa-1erYATzgwQeMCcPuKuMvz-mg9KlwoYeOfGGl41k00EdJ9lUgl31z8",
   })
     .then((currentToken) => {
       if (currentToken) {
